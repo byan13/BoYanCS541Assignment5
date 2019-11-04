c9f4e4672d28f4235a0927e396470cdbf3c19676
Bo Yan <byan2@binghamton.edu>
2019-11-04 09:51:42 -0500

    First commit

---
+++ Project 5/Project 5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 5
+//
+//  Created by Bo Yan on 11/4/19.
+//  Copyright © 2019 Bo Yan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
